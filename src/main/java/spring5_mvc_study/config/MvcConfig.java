cc61cb9c035fa5d340469469a8e85991df943279
Taehoon <leeth950304@gmail.com>
2021-05-18 11:16:47 +0900

    Spring5 web MVC 최소설정 완료

---
+++ src/main/java/spring5_mvc_study/config/MvcConfig.java
@@ -0,0 +1,24 @@
+package spring5_mvc_study.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+	
+	/*DispatcherServlet의 매핑경로를 '/' 주었을 때, JSP/HTML/CSS 등을 올바르게 처리하기 위한 설정 추가 */
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	
+	/* JSP를 통해서 컨트롤러의 실행 결과를 보여주기 위한 설정 */
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/view/",".jsp");
+	}
+}
